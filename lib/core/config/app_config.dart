03508eec3ae220cc746e6f62227212bd6883bfa7
sergio001g <sergio.gomesc13@outlook.es>
2026-07-11 15:53:26 +0200

    chore(config): switch baseUrl to localhost dynamic configuration

--- lib/core/config/app_config.dart
+++ lib/core/config/app_config.dart
@@ -1,5 +1,18 @@
+import 'dart:io' show Platform;
+import 'package:flutter/foundation.dart' show kIsWeb;
+
 class AppConfig {
-  static const String baseUrl = 'https://codeacademy-api.uaeftt-ute.site/api';
+  static String get baseUrl {
+    if (kIsWeb) {
+      return 'http://127.0.0.1:8000/api';
+    }
+    try {
+      if (Platform.isAndroid) {
+        return 'http://10.0.2.2:8000/api'; // IP del host en emulador de Android
+      }
+    } catch (_) {}
+    return 'http://127.0.0.1:8000/api'; // iOS o escritorio
+  }
   
   // Endpoints
   static const String loginEndpoint = '/auth/login/';
