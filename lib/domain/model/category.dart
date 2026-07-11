5f9e2d914188e9aa143304ae1ca8d0b868d9baf7
JulanT0811 <jtlengua.9b@gmail.com>
2026-07-11 15:41:36 +0200

    feat(catalog): add category, subcategory and course domain models

---
+++ lib/domain/model/category.dart
@@ -0,0 +1,27 @@
+class Category {
+  final int id;
+  final String name;
+  final String slug;
+
+  Category({
+    required this.id,
+    required this.name,
+    required this.slug,
+  });
+
+  factory Category.fromJson(Map<String, dynamic> json) {
+    return Category(
+      id: json['id'] as int,
+      name: json['name'] as String? ?? '',
+      slug: json['slug'] as String? ?? '',
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+      'slug': slug,
+    };
+  }
+}
